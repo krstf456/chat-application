c7dd076ef6bd215af0456bee414178b166294aef
mamta srinivas <mamtasrinivassp@gmail.com>
2020-04-20 19:37:53 +0200

    server router set

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+
+const port = process.env.port || 5000
+const router = require('./router')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', (socket)=>{
+    console.log('new connection established')
+    socket.on('disconnect', ()=>{
+        console.log(`User has left`)
+    })
+})
+
+app.use(router)
+server.listen (port, ()=> console.log(`Server is listening on ${port}`))
