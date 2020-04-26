c38992c9843e745937e24145c1d796fd3a0f03a9
mamta srinivas <mamtasrinivassp@gmail.com>
2020-04-26 09:59:57 +0200

    Cleaned

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import './Input.css'
 import { Box, Button, TextInput } from 'grommet'
-
 
 const Input = ({ message, setMessage, sendMessage }) => {
     return (
-        
-
         <Box 
        direction='row'
-       
        >
             <Box
-            
             width='medium'
             >
                 <TextInput 
@@ -22,32 +16,16 @@ const Input = ({ message, setMessage, sendMessage }) => {
                 onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null} />
 
             </Box>
-            <Box 
-            
+            <Box    
             justify='center'
             width='small'
             align='center'
             border='all'
             background='brand'
             >
-
                 <Button  onClick={(event) => sendMessage(event)}>Send</Button>  
             </Box>
-                
-            
         </Box>
-        
-        // <from>
-        //     <input
-        //         className='input'
-        //         type='text'
-        //         placeholder='Type your message here...'
-        //         value={message}
-        //         onChange={(event) => setMessage(event.target.value)}
-        //         onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null}
-        //     />
-        //     <button className='sendButton' onClick={(event) => sendMessage(event)}>Send</button>
-        // </from>
     )
 }
 
