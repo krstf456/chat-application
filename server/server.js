9037ef7c18084c898d45612834d91a3e6f2a14a9
Fluffinator97 <philip.arvidsson97@gmail.com>
2020-04-27 11:43:01 +0200

    Some things

--- server/server.js
+++ server/server.js
@@ -40,7 +40,25 @@ io.on('connection', (socket) => {
         callback()
     })
 
+    // when the client disconnects, we broadcast it to others
+    socket.on('disconnect', () => {
+        console.log(`User has left`)
+        const user = removeUser(socket.id);
+        if (user) {
+            socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left` })
+            io.to(user.room).emit('roomNames', { room: user.room, users: getUsersInRoom(user.room) });
+        }
+    })
 
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', () => {
+        socket.broadcast.to(user.room).emit('typing', { user: user.name })
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', () => {
+        socket.broadcast.to(user.room).emit('stop typing', { user: user.name })
+    });
 })
 
 app.use(router)
